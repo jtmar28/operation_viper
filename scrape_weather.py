c194b5eabfd3c9c7e0225df20c7422c01cdf8a17
Dean Lorenzo <62883239+dlorenzo2000@users.noreply.github.com>
2023-04-02 14:34:06 -0500

    added get_weather_dictionary method

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,3 +1,4 @@
+# scrape_weather.py file start
 from html.parser import HTMLParser 
 import urllib.request
 import datetime
@@ -51,7 +52,7 @@ class WeatherDataParser(HTMLParser):
         # and class="previous disabled"
         elif tag == "li" and ("id", "nav-prev1") in attrs \
             and ("class", "previous disabled") in attrs:
-            self.found_oldest_date = True
+                self.found_oldest_date = True
  
     def handle_endtag(self, tag):
         """
@@ -70,9 +71,9 @@ class WeatherDataParser(HTMLParser):
             # add to weather data
             if self.current_date is not None and self.current_date \
                 not in ["kilometres per hour", "Average", "Extreme"]:
-                self.weather[self.current_date] = self.current_temps
-                self.current_date = None
-                self.current_temps = None
+                    self.weather[self.current_date] = self.current_temps
+                    self.current_date = None
+                    self.current_temps = None
 
         # Check if parser has exited a temperature cell and reset the flag
         elif tag == "td" and self.in_temp_cell:
@@ -109,45 +110,38 @@ class WeatherDataParser(HTMLParser):
         """
 
         return self.rawdata[self.offset:self.offset + self.length].lower()
+    
+    def GetWeatherDictionary(self):
+        """
+        This method returns the entire weather dictionary. 
+        """
+        parser = WeatherDataParser()
+        parser.found_oldest_date = False  # Initialize the flag
+
+        current_month = datetime.datetime.now().month
+        current_date = datetime.datetime.now().day
+        current_year = datetime.datetime.now().year
+
+        year = current_year
+        month = current_month
+        while True:
+            myurl = (f"https://climate.weather.gc.ca/climate_data/daily_data_e.html"
+                    f"?StationID=27174&timeframe=2&StartYear=1840"
+                    f"&EndYear={current_year}&Day=1&Year={year}&Month={month}")
+
+            with urllib.request.urlopen(myurl) as f:
+                html = f.read().decode("utf-8")
+                parser.feed(html)
+                if parser.found_oldest_date:
+                    break
+                month -= 1
+                if month == 0:
+                    month = 12
+                    year -= 1
+
+        return parser.weather
+
 
 if __name__ == "__main__":
-    parser = WeatherDataParser()
-    parser.found_oldest_date = False  # Initialize the flag
-
-    current_month = datetime.datetime.now().month
-    current_date = datetime.datetime.now().day
-    current_year = datetime.datetime.now().year
-
-    print(f"\nToday's date: {current_month}/{current_date}/{current_year}\n")
-
-    year = current_year
-    month = current_month
-    while True:
-        myurl = (f"https://climate.weather.gc.ca/climate_data/daily_data_e.html"
-                 f"?StationID=27174&timeframe=2&StartYear=1840"
-                 f"&EndYear={current_year}&Day=1&Year={year}&Month={month}")
-
-        with urllib.request.urlopen(myurl) as f:
-            html = f.read().decode("utf-8")
-            parser.feed(html)
-            print(f"Processing weather data for {month}/{year}")  
-            
-            # Check if oldest date is found
-            if ('li', 'nav-prev1') in parser.getpos() and ('class', 
-                'previous disabled') in parser.get_starttag_text():
-                parser.found_oldest_date = True
-                break
-
-        # decrement year and month to move to previous month's data
-        month -= 1
-        if month == 0:
-            year -= 1
-            month = 12
-        
-        # Exit the while loop if the oldest date has been found
-        if parser.found_oldest_date:
-            break
-
-    print("Finished processing.")
-    print("Final weather data:")
-    print(parser.weather)
+    weather_dictionary = WeatherDataParser()
+    print(weather_dictionary.GetWeatherDictionary())
