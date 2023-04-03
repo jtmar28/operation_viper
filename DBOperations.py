7044dcd2254238618f10ac40e6351a733728101a
Dean Lorenzo <62883239+dlorenzo2000@users.noreply.github.com>
2023-04-02 21:20:04 -0500

    Fixed bugs, and added functionality to create database, and output a tuple of data within a range of dates. Work still needed.

--- DBOperations.py
+++ DBOperations.py
@@ -1,71 +1,110 @@
+# start of DBOperations.py file
 import sqlite3
-from dbcm import DBCM
+from scrape_weather import WeatherDataParser
+from datetime import datetime
+from pprint import pprint
 
 class DBOperations:
-    """
-    Contains methods to handle database operations, such as initializing table,
-    saving data, purging data, and fetching data.
-    """
-    
-    logger = logging.getLogger("main." + __name__);
-    
-    def __init__(self, weather_database):
-        """Called when object is created. Initializes databaase connection and cursor."""
-        try: 
-            self.weather_database = weather_database # assigns weather_database argument to self.weather_database attribute of DBOperations instance.
-            self.conn = sqlite3.connect(weather_database) # creates a connection to weather_database.
-            self.cursor = self.conn.cursor() # creates a cursor object.
-        except Exception as exception:
-            print("DBOperations:__init__:", exception)
-            self.logger.error("DBOperations:__init__:%s", exception)
-            
-    def __del__(self):
-        """Called when the object is about to be destroyed."""
-        self.cursor.close()
-        self.conn.close()
+    def __init__(self):
+        self.conn = sqlite3.connect("weather_database.sqlite")
+        self.cursor = self.conn.cursor()
+        print("Opened database successfully.") 
 
     def initialize_db(self):
-        """Creates the weather_data table if it does not already exist."""
-        try:
-            with DBCM(self.db_name) as database:
-                database.cursor.execute("""
-                    CREATE TABLE IF NOT EXISTS weather_data (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                        sample_date TEXT NOT NULL,
-                        location TEXT NOT NULL,
-                        min_temp REAL NOT NULL,
-                        max_temp REAL NOT NULL,
-                        avg_temp REAL NOT NULL
-                    );
-                """)
-                database.conn.commit() # Commits the changes to the database.
-        except Exception as exception:
-            print("DBOperations:initialize_db:", exception)
-            self.logger.error("DBOperations:initialize_db:%s", exception)
-
-    def fetch_sample_data(self, location, start_date, end_date):
-        """Fetches the sample data for a given location between two dates."""
-        sql = f"""
-            SELECT sample_date, min_temp, max_temp, avg_temp 
-            FROM weather_data 
-            WHERE location = ? 
-            AND sample_date BETWEEN {start_date} AND {end_date} ?
-        """
-        self.cursor.execute(sql, (location, start_date, end_date))
-        data = self.cursor.fetchall()
-        return data
-    
-    def save_data(self, sample_date, location, min_temp, max_temp, avg_temp):
-        """Inserts and commits data to the weather_data table if it does not already exist."""
-        sql = """
-                  INSERT OR IGNORE INTO weather_data (sample_date, location, min_temp, max_temp, avg_temp)
-                  VALUES (?, ?, ?, ?, ?);
-              """
-        data = (sample_date, location, min_temp, max_temp, avg_temp)
-        self.cursor.execute(sql, data)
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS weather_data (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                sample_date TEXT,
+                location TEXT,
+                max_temp REAL,
+                min_temp REAL,                
+                mean_temp REAL
+            );
+        """) 
         self.conn.commit()
+
+# sample weather dictionary - {“2018-06-01”: {“max_temp”: 12.0, “min_temp”: 5.6, “mean_temp”: 7.1}, “2018-06-02”: {“max_temp”: 12.0, “min_temp”: 5.6, “mean_temp”: 7.1}}
+
+    def save_data(self, data):
+        for sample_date, temps in data.items():
+            location = 'Winnipeg'
+            max_temp = temps.get('max_temp', None)
+            min_temp = temps.get('min_temp', None)          
+            mean_temp = temps.get('mean_temp', None)
+            if max_temp is not None and min_temp is not None and mean_temp is not None:
+                self.cursor.execute("""
+                    SELECT * 
+                    FROM weather_data 
+                    WHERE sample_date=?
+                """, (sample_date,))
+                existing_data = self.cursor.fetchone()
+                if existing_data:
+                    print(f"Data already exists for {sample_date}. Skipping...")
+                else:
+                    try:
+                        self.cursor.execute("""
+                            INSERT INTO weather_data (sample_date, location, max_temp, min_temp, mean_temp)
+                            VALUES (?, ?, ?, ?, ?)
+                        """, (sample_date, location, max_temp, min_temp, mean_temp))
+                        self.conn.commit()
+                        print(f"Data saved for {sample_date}")
+                    except sqlite3.Error as e:
+                        print(f"Error inserting data for {sample_date}: {e}")
+        print('Data saved to the database.')
 
     def purge_data(self):
-        """Deletes all data from the weather_data table."""
         self.cursor.execute('DELETE FROM weather_data')
         self.conn.commit()
+        print('All data purged from the database.')
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+    
+    def fetch_data(self, start_date, end_date):
+        # Convert start and end dates to the correct format for the database
+        start_date_formatted = datetime.strptime(start_date, '%Y-%m-%d').strftime('%B %d, %Y')
+        end_date_formatted = datetime.strptime(end_date, '%Y-%m-%d').strftime('%B %d, %Y')
+
+        # Execute the query using the formatted dates
+        self.cursor.execute("""
+            SELECT sample_date, max_temp, min_temp, mean_temp 
+            FROM weather_data 
+            WHERE sample_date >= ? AND sample_date <= ?
+        """, (start_date_formatted, end_date_formatted))
+
+        # Return the fetched data
+        return self.cursor.fetchall()
+
+def create_entire_database():
+    # Create a WeatherDataParser object to get the weather data
+    parser = WeatherDataParser()
+    # Create a DBOperations object to save the weather data to the database
+    db = DBOperations()
+    db.initialize_db()
+    db.save_data(parser.get_weather_dictionary())
+    db.close()
+
+if __name__ == "__main__":
+
+    # create_entire_database()
+
+    # Create a DBOperations object to fetch weather data from the database
+    db = DBOperations()
+
+    # Prompt the user for start and end dates
+    # start_date = input("Enter start date (YYYY-MM-DD): ")
+    # end_date = input("Enter end date (YYYY-MM-DD): ")
+
+    # Fetch data from the database for the specified date range
+    # data = db.fetch_data(start_date, end_date)
+    data = db.fetch_data("2023-03-01", "2023-03-10")
+
+    # Output data to the screen in the form of a tuple
+    pprint(tuple(data))
+
+    # Close the database connection
+    db.close() 
+ 
+# end of DBOperations.py 
+       
