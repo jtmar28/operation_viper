834438d9804064151495fdefc71a8ad18d819c16
Dean Lorenzo <62883239+dlorenzo2000@users.noreply.github.com>
2023-04-05 14:03:30 -0500

    Added update_database() method.
    Still work in progress. It isn't working as intended yet.

--- db_operations.py
+++ db_operations.py
@@ -24,8 +24,8 @@ class DBOperations:
         """
         This constructor creates a connection and cursor for the database.
         """
-        self.conn = sqlite3.connect("weather_database.sqlite")
-        self.cursor = self.conn.cursor()
+
+        self.cursor = DBCM("weather_database.sqlite")        
         print("Opened database successfully.")
 
     def initialize_db(self):
@@ -33,141 +33,178 @@ class DBOperations:
         Initializes the weather_data table in the database with the necessary columns.
         """
 
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS weather_data (
-                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                sample_date TEXT,
-                location TEXT,
-                max_temp REAL,
-                min_temp REAL,                
-                mean_temp REAL
-            );
-        """)
-        self.conn.commit()
+        with self.cursor as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS weather_data (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    sample_date TEXT,
+                    location TEXT,
+                    max_temp REAL,
+                    min_temp REAL,                
+                    mean_temp REAL
+                );
+            """) 
+            cur.connection.commit()
 
     def save_data(self, data):
         """
         Saves weather data to the database for each date in the given dictionary.
         """
 
-        for sample_date, temps in data.items():
-            location = 'Winnipeg'
-            max_temp = temps.get('max_temp', None)
-            min_temp = temps.get('min_temp', None)
-            mean_temp = temps.get('mean_temp', None)
+        with self.cursor as cur:
+            for sample_date, temps in data.items():
+                location = 'Winnipeg'
+                max_temp = temps.get('max_temp', None)
+                min_temp = temps.get('min_temp', None)
+                mean_temp = temps.get('mean_temp', None)
 
-            if max_temp is not None and min_temp is not None and mean_temp is not None:
-                self.cursor.execute("""
-                    SELECT * 
-                    FROM weather_data 
-                    WHERE sample_date=?
-                """, (sample_date,))
-                existing_data = self.cursor.fetchone()
+                if max_temp is not None and min_temp is not None and mean_temp is not None:
+                    cur.execute("""
+                        SELECT * 
+                        FROM weather_data 
+                        WHERE sample_date=?
+                    """, (sample_date,))
+                    existing_data = cur.fetchone()
 
-                if existing_data:
-                    print(f"Data already exists for {sample_date}. Skipping...")
-                else:
-                    try:
-                        self.cursor.execute("""
-                            INSERT INTO weather_data 
-                            (sample_date, 
-                            location,
-                            max_temp, 
-                            min_temp, 
-                            mean_temp)
-                            VALUES (?, ?, ?, ?, ?)
-                        """, (sample_date,
-                              location,
-                              max_temp,
-                              min_temp,
-                              mean_temp))
-                        self.conn.commit()
-                        print(f"Data saved for {sample_date}")
-                    except sqlite3.Error as e:
-                        print(f"Error inserting data for {sample_date}: {e}")
+                    if existing_data:
+                        print(f"Data already exists for {sample_date}. Skipping...")
+                    else:
+                        try:
+                            self.cursor.execute("""
+                                INSERT INTO weather_data 
+                                (sample_date, 
+                                location,
+                                max_temp, 
+                                min_temp, 
+                                mean_temp)
+                                VALUES (?, ?, ?, ?, ?)
+                            """, (sample_date,
+                                location,
+                                max_temp,
+                                min_temp,
+                                mean_temp))
+                            cur.connection.commit()
+                            print(f"Data saved for {sample_date}")
+                        except sqlite3.Error as e:
+                            print(f"Error inserting data for {sample_date}: {e}")
         print('Data saved to the database.')
 
     def purge_data(self):
         """
         Deletes all data from the weather_data table in the database.
         """
-        self.cursor.execute('DELETE FROM weather_data')
-        self.conn.commit()
-        print('All data purged from the database.')
 
-    def close(self):
-        """
-        Closes the database connection.
-        """
-
-        self.cursor.close()
-        self.conn.close()
-
+        with self.cursor as cur:
+            cur.execute('DELETE FROM weather_data')
+            cur.connection.commit()
+            print('All data purged from the database.')
+        
     def fetch_data(self, start_date, end_date):
         """
         This method fetches the data from the database according to what the 
         user inputs for the start and end dates.
         """
+
         # Convert start and end dates to the correct format for the database
         start_date_formatted = datetime.strptime(start_date, '%Y-%m-%d')
         end_date_formatted = datetime.strptime(end_date, '%Y-%m-%d')
 
-        # Check to see if there are any new records that haven't been added to the 
-        # database yet
-
-        parser = WeatherDataParser()
-        
-        
-
-
-
-
-
-
-
-
-
-
-
-
         # Create an empty list to store the temperature data for each date
         data = []
 
-        # Loop through the dates and fetch the temperature data for each date
-        while start_date_formatted <= end_date_formatted:
-            sample_date = start_date_formatted.strftime('%B %d, %Y')
-            # Drop leading zeroes only from the day portion of the formatted date
-            sample_date = re.sub(r'(?<=\s)0', '', sample_date)
-            self.cursor.execute("""
-                SELECT max_temp, min_temp, mean_temp 
-                FROM weather_data 
-                WHERE sample_date = ?
-            """, (sample_date,))
-            result = self.cursor.fetchone()
+        with self.cursor as cur:
 
-            # If the temperature data for the date exists, add it to the list
-            if result:
-                max_temp, min_temp, mean_temp = result
-                data.append((sample_date, max_temp, min_temp, mean_temp))
+            # Loop through the dates and fetch the temperature data for each date
+            while start_date_formatted <= end_date_formatted:
+                sample_date = start_date_formatted.strftime('%B %d, %Y')
 
-            # Increment the date by one day
-            start_date_formatted += timedelta(days=1)
+                # Drop leading zeroes only from the day portion of the formatted date
+                sample_date = re.sub(r'(?<=\s)0', '', sample_date)
+                cur.execute("""
+                    SELECT max_temp, min_temp, mean_temp 
+                    FROM weather_data 
+                    WHERE sample_date = ?
+                """, (sample_date,))
+                result = cur.fetchone()
+
+                # If the temperature data for the date exists, add it to the list
+                if result:
+                    max_temp, min_temp, mean_temp = result
+                    data.append((sample_date, max_temp, min_temp, mean_temp))
+
+                # Increment the date by one day
+                start_date_formatted += timedelta(days=1)
 
         # Return the list of temperature data as a tuple
         return tuple(data)
 
-def create_entire_database():
-    """
-    This function creates the database for the first time in order to retrieve data.
-    """
-    # Create a WeatherDataParser object to get the weather data
-    parser = WeatherDataParser()
 
-    # Create a DBOperations object to save the weather data to the database
-    db = DBOperations()
-    db.initialize_db()
-    db.save_data(parser.get_weather_dictionary())
-    db.close()
+    def create_entire_database(self):
+        """
+        This function creates the database for the first time in order to retrieve data.
+        """
+
+        # Create a WeatherDataParser object to get the weather data
+        parser = WeatherDataParser()
+
+        # Create a DBOperations object to save the weather data to the database
+        self.initialize_db()
+        self.save_data(parser.get_weather_dictionary())
+        print("Entire database created and records added.")
+
+    
+    def update_database(self):
+        """
+        Updates the weather_data table in the database with the latest data 
+        from the WeatherDataParser if it is more recent than the latest data 
+        in the table.
+        """
+
+        # Get the latest date in the database
+        with self.cursor as cur:
+            cur.execute("""
+                SELECT sample_date
+                FROM weather_data
+                WHERE id =  (SELECT min(id) 
+                             FROM weather_data)
+            """)            
+            latest_date = cur.fetchone()[0]
+
+        print(f"The newest record in the database is {latest_date}")
+        
+        # Drop leading zeroes only from the day portion of the formatted date
+        # latest_date = re.sub(r'(?<=\s)0', '', latest_date)
+        
+        latest_date = datetime.strptime(latest_date, '%B %d, %Y')
+
+        # Format date to compare to today's date
+        latest_date = latest_date.strftime('%y-%m-%d')
+            
+        # Get today's date in 'YYYY-MM-DD' format
+        today = datetime.now().strftime('%Y-%m-%d')
+
+        print(f"Today's date is {today}")
+
+        if(latest_date < today):
+            print("The latest date is older. ")
+        else:
+            print("Something is up")
+
+        # Check if the latest date in the database is older than today's date
+        if latest_date < today:
+            # Create a WeatherDataParser object to get the weather data
+            parser = WeatherDataParser()
+
+            # Get the new data and save it to the database 
+            new_data = parser.check_for_new_data(latest_date.strftime('%Y-%m-%d'), today)
+            self.save_data(new_data)
+
+            # Output how many new records were added to the database
+            num_new_records = len(new_data)
+            print(f"{num_new_records} new records added to the database.")
+        else:
+            print("Database is up to date.")
+        
 
 if __name__ == "__main__":
     """
@@ -175,21 +212,20 @@ if __name__ == "__main__":
     functionality to parse initial data to the database. 
     """
 
-    # create_entire_database()
-
-    # Create a DBOperations object to fetch weather data from the database
+    # # Create a DBOperations object to fetch weather data from the database
     db = DBOperations()
+    # db.create_entire_database()
 
-    # Prompt the user for start and end dates
-    start_date = input("Enter start date (YYYY-MM-DD): ")
-    end_date = input("Enter end date (YYYY-MM-DD): ")
+    # Check to see if the database has as new records to add
+    db.update_database()
 
-    # Fetch data from the database for the specified date range
-    data = db.fetch_data(start_date, end_date)
+    # # Prompt the user for start and end dates
+    # start_date = input("Enter start date (YYYY-MM-DD): ")
+    # end_date = input("Enter end date (YYYY-MM-DD): ")
 
-    # Output data to the screen in the form of a tuple
-    print(data)
+    # # Fetch data from the database for the specified date range
+    # data = db.fetch_data(start_date, end_date)
 
-    # Close the database connection
-    db.close()
+    # # Output data to the screen in the form of a tuple
+    # pprint(data)   
        
