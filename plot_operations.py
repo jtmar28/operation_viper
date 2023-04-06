f4476dba5d1e29d0f384ccb949e2b181bbd8eb02
dlorenzo2000 <deanlorenzo@gmail.com>
2023-04-06 11:45:34 -0500

    Added progress to code.

--- plot_operations.py
+++ plot_operations.py
@@ -9,10 +9,8 @@ class PlotOperations:
         self.year_end = ""
         self.month_start_date = ""
         self.month_end_date = ""
-        pass
 
     def fetch_yearly_averages(self):
-
         # get user input
         # self.year_start = input("Enter a start year (YYYY): ")
         # self.year_end = input("Enter an end year (YYYY): ")
@@ -29,19 +27,15 @@ class PlotOperations:
         return tuple(yearly_weather_data)
 
     def fetch_month_averages(self):
-
         # get user input
         # self.month_start_date = input("Enter a start date (YYYY-MM-DD): ")
         # self.month_end_date = input("Enter an end date (YYYY-MM-DD): " )
-
         
         # hard-coded data for testing
         self.month_start_date = "2023-1-1"
         self.month_end_date = "2023-1-31"
 
         # get data for one month for plotting dates hardcoded for now
-        # self.month_start_date = "2023-1-1"
-        # self.month_end_date = "2023-1-31"
         one_month__weather_data = db.fetch_data(self.month_start_date, self.month_end_date)
 
         return tuple(one_month__weather_data)
@@ -51,4 +45,6 @@ if __name__ == "__main__":
     db = DBOperations()
     plot = PlotOperations()
 
+    # print(plot.fetch_month_averages())
+    print(plot.fetch_yearly_averages())
 
