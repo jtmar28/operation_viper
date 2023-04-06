63a626cc0225c9de0e844c6517d4c1170139ace6
dlorenzo2000 <deanlorenzo@gmail.com>
2023-04-06 11:40:08 -0500

    Added methods to fetch monthly and yearly averages

--- plot_operations.py
+++ plot_operations.py
@@ -3,23 +3,52 @@ import numpy as np
 from db_operations import DBOperations
 from pprint import pprint
 
+class PlotOperations:
+    def __init__(self):
+        self.year_start = ""
+        self.year_end = ""
+        self.month_start_date = ""
+        self.month_end_date = ""
+        pass
+
+    def fetch_yearly_averages(self):
+
+        # get user input
+        # self.year_start = input("Enter a start year (YYYY): ")
+        # self.year_end = input("Enter an end year (YYYY): ")
+
+        # hard-coded data for testing
+        self.year_start = "2015"
+        self.year_end = "2023"
+
+        # get data for several years for plotting dates hardcoded for now
+        year_start_date = f"{self.year_start}-1-1"
+        year_end_date = f"{self.year_end}-12-31"
+        yearly_weather_data = db.fetch_data(year_start_date, year_end_date)
+
+        return tuple(yearly_weather_data)
+
+    def fetch_month_averages(self):
+
+        # get user input
+        # self.month_start_date = input("Enter a start date (YYYY-MM-DD): ")
+        # self.month_end_date = input("Enter an end date (YYYY-MM-DD): " )
+
+        
+        # hard-coded data for testing
+        self.month_start_date = "2023-1-1"
+        self.month_end_date = "2023-1-31"
+
+        # get data for one month for plotting dates hardcoded for now
+        # self.month_start_date = "2023-1-1"
+        # self.month_end_date = "2023-1-31"
+        one_month__weather_data = db.fetch_data(self.month_start_date, self.month_end_date)
+
+        return tuple(one_month__weather_data)
+    
 
 if __name__ == "__main__":
     db = DBOperations()
+    plot = PlotOperations()
 
-    # start_date = input("Enter a start date (YYYY-MM-DD): ")
-    # end_date = input("Enter an end date (YYYY-MM-DD): ")
-
-    # get data for several years for plotting dates hardcoded for now
-    start_date = "2015-1-1"
-    end_date = "2018-12-31"
-    yearly_weather_data = db.fetch_data(start_date, end_date)
-
-    # get data for one month for plotting dates hardcoded for now
-    start_date = "2023-1-1"
-    end_date = "2023-1-31"
-    one_month__weather_data = db.fetch_data(start_date, end_date)
-
-    print(yearly_weather_data)
-    print(one_month__weather_data)
 
