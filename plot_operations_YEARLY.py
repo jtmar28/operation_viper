c65a81c8cdb6e1557129945cf4507e17dc33a25f
dlorenzo2000 <deanlorenzo@gmail.com>
2023-04-08 13:43:24 -0500

    Added monthly data mean calculation

--- plot_operations_YEARLY.py
+++ plot_operations_YEARLY.py
@@ -3,6 +3,7 @@ import numpy as np
 from db_operations import DBOperations
 from pprint import pprint
 from datetime import datetime
+import statistics
 
 class PlotOperations:
     def __init__(self):
@@ -11,13 +12,13 @@ class PlotOperations:
         self.month_start_date = ""
         self.month_end_date = ""
 
-    def fetch_yearly_averages(self):
+    def fetch_monthly_year_averages(self, month):
         # get user input
         # self.year_start = input("Enter a start year (YYYY): ")
         # self.year_end = input("Enter an end year (YYYY): ")
 
         # hard-coded data for testing
-        self.year_start = "2015"
+        self.year_start = "2020"
         self.year_end = "2023"
 
         # get data for several years for plotting dates hardcoded for now
@@ -25,8 +26,29 @@ class PlotOperations:
         year_end_date = f"{self.year_end}-12-31"
         yearly_weather_data = dict(db.fetch_mean_temp(year_start_date, year_end_date))
 
-        months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  
+        new_dict = {}
+        values = []
+        outliers = []
+
+        for key, value in yearly_weather_data.items():
+            if month in key:
+                values.append(float(value[0]))
+                new_dict[key] = values[-1]
+
+        # accuracy to 1 decimal point        
+        mean = round(statistics.mean(values), 1)
+        if (mean > 0):
+            box_top = round(mean * 1.25, 1)
+            box_bottom = round(mean * 0.75, 1)
+            maximum = round(1.25 * box_top, 1)
+            minimum = round(box_bottom * 0.75, 1)
+        else:
+            box_bottom = round(mean * 1.25, 1)
+            box_top = round(mean * 0.75, 1)
+            minimum = round(.75 * box_top, 1)
+            maximum = round(box_bottom * 1.25, 1)
+            
+        print(f"{month}: mean = {mean} box_top = {box_top} box_bottom = {box_bottom} maximum = {maximum} minimum = {minimum}")
 
         return yearly_weather_data
 
@@ -54,5 +76,6 @@ if __name__ == "__main__":
     db = DBOperations()
     plot = PlotOperations()
 
-    print(plot.fetch_yearly_averages()) 
+    # fetch average of the month of February
+    plot.fetch_monthly_year_averages('January')
 
